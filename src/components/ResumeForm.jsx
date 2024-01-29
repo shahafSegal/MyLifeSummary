cbbb248eda543d6699038d54c7db3ee6ac15b366
shahafSegal <goigadol360@gmail.com>
2024-01-29 16:01:21 +0200

    added style on change

--- src/components/ResumeForm.jsx
+++ src/components/ResumeForm.jsx
@@ -1,31 +1,57 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { getObjHandleForm } from '../scripts/general'
+import '../styles/resumeForm.css'
 
 export default function ResumeForm(props) {
     // const preExistData=props.resumeData;
     // const [WorkCards,SetWorkCard]=useState(preExistData?)
+    const [FirstFormObj,SetFirstFormObj]=useState({})
+    const [WorkFormObj,SetWorkForm]=useState([])
+    const [EduFormObj,SetEduForm]=useState([])
+    const fFormRef=useRef()
 
-    const mainsubmitFunc=(e)=>{
+    useEffect(()=>{
+        const formObj= Object.fromEntries( new FormData( fFormRef.current))
+        SetFirstFormObj(formObj)
+    },[])
+    
+
+    const firstSubmitFunc=(e)=>{
         const formObj= getObjHandleForm(e)
-        console.log(formObj)
-
+        SetFirstFormObj(formObj)
+        fFormRef.current.classList.remove('changedForm')
     }
 
+    const sendBigForm=()=>{
+        let newResume={}
+        newResume={...newResume,...FirstFormObj}
+        newResume={...newResume,work:WorkFormObj,education:EduFormObj}
+        props.resumeChange(newResume)
+    }
+
+    const fFormChange=()=>{
+        fFormRef.current.classList.add('changedForm')
+    }
+
+
   return (
-    <form onSubmit={mainsubmitFunc}>
-        <label htmlFor="fullname">fullname:
-            <input type="text" name="fullname"/>
-        </label>
-        <label htmlFor="about">about:
-            <input type="text" name="about"/>
-        </label>
-        <label htmlFor="phone">phone:
-            <input type="number" name="phone"/>
-        </label>
-        <label htmlFor="email">email:
-            <input type="email" name="email" />
-        </label>
-        <button type="submit">submit</button>
-    </form>
+    <div>
+        <form ref={fFormRef} onSubmit={firstSubmitFunc}>
+            <label htmlFor="fullname">fullname:
+                <input onChange={fFormChange} type="text" name="fullname"/>
+            </label>
+            <label htmlFor="about">about:
+                <input onChange={fFormChange} type="text" name="about"/>
+            </label>
+            <label htmlFor="phone">phone:
+                <input onChange={fFormChange} type="number" name="phone"/>
+            </label>
+            <label htmlFor="email">email:
+                <input onChange={fFormChange} type="email" name="email" />
+            </label>
+            <button type="submit">submit</button>
+        </form>
+        <button onClick={sendBigForm}>save new resume</button>
+    </div>
   )
 }
--- src/components/pages/CreateResume.jsx
+++ src/components/pages/CreateResume.jsx
@@ -1,26 +1,62 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import ResumeForm from '../ResumeForm'
 import { getObjHandleForm } from '../../scripts/general'
+import {addDoc, collection} from 'firebase/firestore'
+import { db } from '../../config/config'
+import { UserContext } from '../../contexts/UserManager'
 
 export default function CreateResume() {
+    const {UserObj}=useContext(UserContext)
+    const userId=UserObj.id;
+    const[IsResumeSent,SetIsResumeSend]=useState(false);
+    const[CurrResume,SetCurrResume]=useState({})
+    useEffect(()=>{console.log(CurrResume)
+        if(CurrResume.work){
+            console.log("sending now")
+        }
+    },[CurrResume])
 
-    const [FormName,SetFormName]=useState('')
+    async function addResumeDb(){
+        {
+          try{
+            const docRef= await addDoc(collection(db,"resumes"),
+            {...CurrResume,userId:userId
+            }
+            )
+            SetIsResumeSend(true)
+          }catch (error) {
+            console.error("Error setting resumes:", error);
+          }
+        }
+    }
+    const anotherResume=()=>{
+        SetIsResumeSend(false)
+        SetCurrResume({})
+    }
+
 
     const getName=(e)=>{
         const formObj= getObjHandleForm(e)
-        SetFormName(formObj.name)
+        SetCurrResume({...CurrResume,...formObj})
+    }
+    const changeCurrResume=(newResumeData)=>{
+        SetCurrResume({...CurrResume,...newResumeData})
     }
 
 
   return (
     <div>
-        <form onSubmit={getName}>
-            <label htmlFor="name">name for form:
-                <input type="text" name='name' />
-            </label>
-            <button type="submit">{FormName?'add':'change'} resume name</button>
-        </form>
-        {FormName?[<ResumeForm key={FormName} />,<button>save new resume</button>]:<h2>Name Resume to start filling</h2>}
+        {IsResumeSent?[<h1>Resume created Succesfully</h1>,<button onClick={anotherResume}>Create Another Resume</button>]:<> <form onSubmit={getName}>
+           
+                <label htmlFor="docName">name for form:
+                    <input type="text" name='docName' />
+                </label>
+                <button type="submit">{CurrResume.docName?'change':'add'} resume name</button>
+            </form>
+            {CurrResume.docName?<ResumeForm resumeChange={changeCurrResume}/>:<h2>Name Resume to start filling</h2>}
+            {CurrResume.work?<button onClick={addResumeDb}> send Doc</button>:null}
+            </>
+        }
         
     </div>
     
